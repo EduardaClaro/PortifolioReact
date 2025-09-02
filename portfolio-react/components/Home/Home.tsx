aec6c7c4187fa98f61eeb26efcbe52bea9c126bc
EduardaClaro <mariaeduardaclaro06@gmali.com>
2025-09-02 15:34:10 -0300

    :sparkles: feat: linking to the hero

--- portfolio-react/components/Home/Home.tsx
+++ portfolio-react/components/Home/Home.tsx
@@ -1,7 +1,10 @@
 import React from "react";
+import Hero from "./Hero/Hero";
 
 const Home = () => {
-  return <div className="overflow-hidden h-[10000]"></div>
+  return <div className="overflow-hidden h-[10000]">
+    <Hero/>
+  </div>
 };
 
 export default Home;
