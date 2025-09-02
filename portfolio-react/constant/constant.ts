04a0177034cdb488b9ead42040f3b9ae43de684e
EduardaClaro <mariaeduardaclaro06@gmali.com>
2025-09-02 09:34:05 -0300

    :sparkles: feat: content reduction

---
+++ portfolio-react/constant/constant.ts
@@ -0,0 +1,27 @@
+import { url } from "inspector";
+
+export const NavLinks = [
+  {
+    id: 1,
+    url: "#",
+    Label: "Home",
+  },
+
+  {
+    id: 2,
+    url: "#",
+    Label: "Resumo",
+  },
+
+  {
+    id: 3,
+    url: "#",
+    Label: "Projetos",
+  },
+
+  {
+    id: 4,
+    url: "#",
+    Label: "Contatos",
+  },
+];
