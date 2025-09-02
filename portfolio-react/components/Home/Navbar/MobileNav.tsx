9979b1de9d8e02c6af44f239a65be2d1e3bca2a3
EduardaClaro <mariaeduardaclaro06@gmali.com>
2025-09-02 15:31:23 -0300

    :lipstick: feat: updated styling

--- portfolio-react/components/Home/Navbar/MobileNav.tsx
+++ portfolio-react/components/Home/Navbar/MobileNav.tsx
@@ -9,34 +9,49 @@ type MobileNavProps = {
 };
 
 const MobileNav: React.FC<MobileNavProps> = ({ showNav, closeNav }) => {
+
+  const navOpen = showNav ? "translate-x-0" : "translate-x-[100%]";
+
   return (
     <>
       {/* Overlay */}
       <div
-        className={`fixed inset-0 transition-opacity duration-500 z-[100002] bg-slate-950 ${
-          showNav ? "opacity-70 visible" : "opacity-0 invisible"
-        }`}
+        className={`fixed inset-0 ${navOpen} transform transition-all right-0 duration-500 z-[100002] bg-slate-900 
+        opacity-70 w-full h-screen`}
         onClick={closeNav}
       />
 
       {/* navLinks */}
       <div
-        className={`text-white fixed justify-center flex flex-col h-full transform transition-transform duration-500 w-[80%] sm:w-[60%] bg-slate-800 space-x-6 z-[100050] right-0 ${
-          showNav ? "translate-x-0" : "translate-x-full"
-        }`}
+        className={`text-white ${navOpen} fixed justify-center flex flex-col h-full transform transition-all duration-500 delay-300 w-[80%] sm:w-[60%] bg-slate-800 space-y-6 z-[100050] right-0 ${
+        showNav ? "translate-x-0" : "translate-x-full"
+      }`}
+
       >
         {NavLinks.map((link) => (
           <Link key={link.id} href={link.url}>
-            <p className="text-white w-fit text-xl ml-12 border-b-[1.5px] pb-1 border-white sm:text-[30px]">
+            <p className="text-white hover:text-cyan-300 w-fit text-xl ml-12 border-b-[1.5px] pb-1 border-cyan-300 sm:text-[30px]">
               {link.Label}
             </p>
           </Link>
         ))}
 
         {/* Cross Icon */}
-        <CgClose>
+        <button
+          type="button"
           onClick={closeNav}
-        </CgClose>
+          style={{
+            position: "absolute",
+            top: 16,
+            right: 16,
+            background: "transparent",
+            border: "none",
+            cursor: "pointer",
+          }}
+          aria-label="Fechar menu"
+        >
+          <CgClose size={28} color="white" />
+        </button>
 
       </div>
     </>
