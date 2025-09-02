1c49e28459206f82f93194ef797953a0ea628d93
EduardaClaro <mariaeduardaclaro06@gmali.com>
2025-09-02 09:27:15 -0300

    :sparkles: feat: code changes and improvements

--- portfolio-react/components/Home/Navbar/MobileNav.tsx
+++ portfolio-react/components/Home/Navbar/MobileNav.tsx
@@ -1,7 +1,46 @@
-import React from 'react'
+import { NavLinks } from '@/constant/constant';
+import React from 'react';
+import Link from "next/link";
+import { CgClose } from 'react-icons/cg';
 
-const MobileNav = () => {
-  return <div>MobileNav</div>
+type MobileNavProps = {
+  showNav: boolean;
+  closeNav: () => void;
+};
+
+const MobileNav: React.FC<MobileNavProps> = ({ showNav, closeNav }) => {
+  return (
+    <>
+      {/* Overlay */}
+      <div
+        className={`fixed inset-0 transition-opacity duration-500 z-[100002] bg-slate-950 ${
+          showNav ? "opacity-70 visible" : "opacity-0 invisible"
+        }`}
+        onClick={closeNav}
+      />
+
+      {/* navLinks */}
+      <div
+        className={`text-white fixed justify-center flex flex-col h-full transform transition-transform duration-500 w-[80%] sm:w-[60%] bg-slate-800 space-x-6 z-[100050] right-0 ${
+          showNav ? "translate-x-0" : "translate-x-full"
+        }`}
+      >
+        {NavLinks.map((link) => (
+          <Link key={link.id} href={link.url}>
+            <p className="text-white w-fit text-xl ml-12 border-b-[1.5px] pb-1 border-white sm:text-[30px]">
+              {link.Label}
+            </p>
+          </Link>
+        ))}
+
+        {/* Cross Icon */}
+        <CgClose>
+          onClick={closeNav}
+        </CgClose>
+
+      </div>
+    </>
+  );
 };
 
 export default MobileNav;
