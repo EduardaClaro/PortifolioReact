10615e91f88f92d66475a8cf6e42a41728aa1c8b
EduardaClaro <mariaeduardaclaro06@gmali.com>
2025-09-04 14:08:11 -0300

    :sparkles: feat: adding the Resume to the main file

--- portfolio-react/components/Home/Home.tsx
+++ portfolio-react/components/Home/Home.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import Hero from "./Hero/Hero";
+import Services from "./Services/Services";
+import Resume from "./Resume/Resume";
 
 const Home = () => {
-  return <div className="overflow-hidden h-[10000]">
+  return <div className="overflow-hidden h-auto">
     <Hero/>
+    <Services/>
+    <Resume/>
   </div>
 };
 
