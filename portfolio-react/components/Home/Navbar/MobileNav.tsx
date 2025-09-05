3c4555fa794df4a2aa90af613b8db0f62db8af3e
EduardaClaro <mariaeduardaclaro06@gmali.com>
2025-09-05 09:41:44 -0300

    :ok_hand: style: adjusting details in styling

--- portfolio-react/components/Home/Navbar/MobileNav.tsx
+++ portfolio-react/components/Home/Navbar/MobileNav.tsx
@@ -1,7 +1,14 @@
-import { NavLinks } from '@/constant/constant';
-import React from 'react';
+import { NavLinks } from "@/constant/constant";
+import React from "react";
 import Link from "next/link";
-import { CgClose } from 'react-icons/cg';
+import { CgClose } from "react-icons/cg";
+import { Roboto_Condensed } from "next/font/google";
+
+// Fonte Roboto Condensed
+const robotoCondensed = Roboto_Condensed({
+  subsets: ["latin"],
+  weight: ["400", "600", "700"],
+});
 
 type MobileNavProps = {
   showNav: boolean;
@@ -9,28 +16,29 @@ type MobileNavProps = {
 };
 
 const MobileNav: React.FC<MobileNavProps> = ({ showNav, closeNav }) => {
-
   const navOpen = showNav ? "translate-x-0" : "translate-x-[100%]";
 
   return (
     <>
       {/* Overlay */}
       <div
-        className={`fixed inset-0 ${navOpen} transform transition-all right-0 duration-500 z-[100002] bg-slate-900 
+        className={`fixed inset-0 ${navOpen} transform transition-all right-0 duration-500 z-[100002] bg-purple-950 
         opacity-70 w-full h-screen`}
         onClick={closeNav}
       />
 
       {/* navLinks */}
       <div
-        className={`text-white ${navOpen} fixed justify-center flex flex-col h-full transform transition-all duration-500 delay-300 w-[80%] sm:w-[60%] bg-slate-800 space-y-6 z-[100050] right-0 ${
-        showNav ? "translate-x-0" : "translate-x-full"
-      }`}
-
+        className={`${robotoCondensed.className} text-white ${navOpen} fixed justify-center flex flex-col h-full transform transition-all duration-500 delay-300 w-[80%] sm:w-[60%] bg-[#1e0e2b] space-y-6 z-[100050] right-0 ${
+          showNav ? "translate-x-0" : "translate-x-full"
+        }`}
       >
         {NavLinks.map((link) => (
           <Link key={link.id} href={link.url}>
-            <p className="text-white hover:text-cyan-300 w-fit text-xl ml-12 border-b-[1.5px] pb-1 border-cyan-300 sm:text-[30px]">
+            <p
+              className="text-white hover:text-purple-400 w-fit text-xl ml-12 border-b-[1.5px] pb-1 border-purple-300 
+              sm:text-[30px] transition-all duration-300 ease-in-out hover:scale-110"
+            >
               {link.Label}
             </p>
           </Link>
@@ -49,10 +57,10 @@ const MobileNav: React.FC<MobileNavProps> = ({ showNav, closeNav }) => {
             cursor: "pointer",
           }}
           aria-label="Fechar menu"
+          className="transition-all duration-300 ease-in-out hover:scale-120"
         >
           <CgClose size={28} color="white" />
         </button>
-
       </div>
     </>
   );
--- portfolio-react/components/Home/Navbar/Nav.tsx
+++ portfolio-react/components/Home/Navbar/Nav.tsx
@@ -6,6 +6,13 @@ import { FaCode } from "react-icons/fa";
 import { BiDownload } from "react-icons/bi";
 import { HiBars3BottomRight } from "react-icons/hi2";
 import Link from "next/link";
+import { Roboto_Condensed } from "next/font/google";
+
+// Fonte Roboto Condensed
+const robotoCondensed = Roboto_Condensed({
+  subsets: ["latin"],
+  weight: ["400", "600", "700"],
+});
 
 type Props = {
   openNav: () => void;
@@ -20,24 +27,25 @@ const Nav = ({ openNav }: Props) => {
     };
 
     window.addEventListener("scroll", handler);
-
     return () => window.removeEventListener("scroll", handler);
   }, []);
 
   return (
     <div
       className={`transition-all ${
-        navBg ? "bg-[#01042d] shadow-md" : "fixed"
+        navBg ? "bg-[#2b0936] shadow-md" : "fixed"
       } duration-200 h-[12vh] z-[10000] fixed w-full`}
     >
-      <div className="flex items-center h-full justify-between w-[90%] mx-auto">
+      <div
+        className={`${robotoCondensed.className} flex items-center h-full justify-between w-[90%] mx-auto`}
+      >
         {/* Logo */}
-        <div className="flex items-center space-x-2">
+        <div className="flex items-center space-x-2 transition-all duration-300 ease-in-out hover:scale-110">
           <div className="w-10 h-10 bg-white rounded-full flex items-center justify-center flex-col">
             <FaCode size={20} color="black" />
           </div>
-          <h1 className="text-xl hidden sm:block md:text-2xl text-white font-bold">
-            Portifólio
+          <h1 className="text-2xl hidden sm:block md:text-2xl text-white font-bold">
+            Eduarda Claro
           </h1>
         </div>
 
@@ -47,7 +55,7 @@ const Nav = ({ openNav }: Props) => {
             <Link
               key={link.id}
               href={link.url}
-              className="text-base hover:text-cyan-300 text-white font-medium transition-all duration-200"
+              className="text-2xl hover:text-purple-500 text-white font-semibold transition-all duration-300 ease-in-out hover:scale-110"
             >
               <p>{link.Label}</p>
             </Link>
@@ -57,14 +65,17 @@ const Nav = ({ openNav }: Props) => {
         {/* Buttons */}
         <div className="flex items-center space-x-4">
           {/* CV Button */}
-          <button className="px-8 py-3.5 text-sm cursor-pointer rounded-lg bg-blue-800 hover:bg-blue-900 transition-all duration-300 text-white flex items-center space-x-2">
-            <BiDownload size={20} />
+          <button className="px-6 py-2.5 text-lg cursor-pointer rounded-full bg-purple-900 hover:bg-purple-950 transition-all duration-300 ease-in-out text-white flex items-center space-x-2 hover:scale-105">
+            <BiDownload size={22} />
             <span>Download CV</span>
           </button>
 
           {/* Burger Menu */}
-          <div onClick={openNav} className="cursor-pointer lg:hidden">
-            <HiBars3BottomRight size={25} color="white"/>
+          <div
+            onClick={openNav}
+            className="cursor-pointer lg:hidden transition-all duration-300 ease-in-out hover:scale-110"
+          >
+            <HiBars3BottomRight size={45} color="white" />
           </div>
         </div>
       </div>
