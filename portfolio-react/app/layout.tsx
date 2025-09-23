873bc3a67419e9fc445e284965943c26a899a13e
EduardaClaro <mariaeduardaclaro06@gmali.com>
2025-09-23 10:58:10 -0300

    :iphone: adding responsiveness to all pages

--- portfolio-react/app/layout.tsx
+++ portfolio-react/app/layout.tsx
@@ -1,28 +1,39 @@
+"use client";
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { AnimatePresence, motion } from "framer-motion";
+import { usePathname } from "next/navigation";
+import Footer from "@/components/Home/Footer/Footer";
 import ResponsiveNav from "@/components/Home/Navbar/ResponsiveNav";
 
-const font= Inter({
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+const font = Inter({
+  weight: ['100','200','300','400','500','600','700','800','900'],
   subsets: ['latin'],
-})
-
-export const metadata: Metadata = {
-  title: "Sajal Portifolio | Next.js 15",
-  description: "Portifolio built with Next.js 15",
-};
+});
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: Readonly<{ children: React.ReactNode }>) {
+  const pathname = usePathname(); // pega a rota atual para AnimatePresence
+
   return (
     <html lang="en">
-      <body className={`${font.className} antialiased bg-slate-950`}>
+      <body className={`${font.className} antialiased bg-[#140020]`}>
         <ResponsiveNav/>
-        {children}
+        <AnimatePresence mode="wait">
+          <motion.div
+            key={pathname} // importante para diferenciar páginas
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -20 }}
+            transition={{ duration: 0.4 }}
+          >
+            {children}
+            <Footer/>
+          </motion.div>
+        </AnimatePresence>
       </body>
     </html>
   );
