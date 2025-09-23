d56980dd021196a0dbd80b2aa09f069e87a7c833
EduardaClaro <mariaeduardaclaro06@gmali.com>
2025-09-23 11:05:05 -0300

    :ok_hand: style: reviewing code and improving

--- portfolio-react/components/Home/Hero/Hero.tsx
+++ portfolio-react/components/Home/Hero/Hero.tsx
@@ -22,21 +22,21 @@ const Hero = () => {
     >
       <ParticlesHero />
 
-      <div className="relative z-10 flex flex-col items-center w-full max-w-6xl px-4">
+      <div className="relative z-10 flex flex-col items-center w-[90%] max-w-6xl mx-auto px-4">
         <Image
           src="/assets/avatarDuda.png"
           alt="heroimage"
-          className="max-w-xs md:max-w-md lg:max-w-xl h-auto mb-8 transition-all duration-500 ease-in-out hover:scale-108"
+          className="w-40 sm:w-56 md:w-72 lg:w-96 h-auto mb-8 transition-all duration-500 ease-in-out hover:scale-105"
           width={370}
           height={370}
         />
 
         <div className="text-center">
-          <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-6xl font-bold items-center transition-all duration-300 ease-in-out hover:scale-108 hover:text-purple-300">
+          <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-6xl font-bold transition-all duration-300 ease-in-out hover:scale-105 hover:text-purple-300">
             Sejam <span className="text-purple-500">Bem Vindos</span>
           </h1>
 
-          <h2 className="mt-5 text-sm sm:text-2xl font-medium flex items-center justify-center gap-2 transition-all duration-300 ease-in-out hover:text-purple-300">
+          <h2 className="mt-5 text-base sm:text-xl md:text-2xl font-medium flex items-center justify-center gap-2 transition-all duration-300 ease-in-out hover:text-purple-300">
             Portfólio
             <span className="text-purple-500 font-bold">
               <Typewriter
@@ -54,17 +54,7 @@ const Hero = () => {
           </h2>
         </div>
 
-        <div className="mt-6 flex gap-4">
-          {/* Botão Meus Projetos */}
-          <button
-            className="px-6 py-2 bg-purple-900 hover:bg-purple-950 transition-all duration-300 ease-in-out
-            cursor-pointer rounded-full text-lg font-medium flex items-center justify-center hover:scale-108"
-          >
-            <span>Meus Projetos</span>
-            <span className="w-5 h-5 ml-2 inline-block">
-              <BsArrowRight />
-            </span>
-          </button>
+        <div className="mt-6 flex flex-col sm:flex-row gap-4">
         </div>
       </div>
     </div>
