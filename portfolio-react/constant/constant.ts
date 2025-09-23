1ee27eacc7a4f2deb34d24eacf34a96ddbf6dedc
EduardaClaro <mariaeduardaclaro06@gmali.com>
2025-09-23 11:15:21 -0300

    :sparkles: feat: adding new paths

--- portfolio-react/constant/constant.ts
+++ portfolio-react/constant/constant.ts
@@ -1,33 +1,10 @@
 import { url } from "inspector";
 
 export const NavLinks = [
-  {
-    id: 1,
-    url: "#",
-    Label: "Home",
-  },
-
-  {
-    id: 2,
-    url: "#",
-    Label: "Competências",
-  },
-
-  {
-    id: 3,
-    url: "#",
-    Label: "Resumo",
-  },
-
-  {
-    id: 4,
-    url: "#",
-    Label: "Projetos",
-  },
-
-  {
-    id: 5,
-    url: "#",
-    Label: "Contatos",
-  },
+  { id: 1, url: "Hero", Label: "Início" },
+  { id: 2, url: "Services", Label: "Competências" },
+  { id: 3, url: "Resume", Label: "Resumo" },
+  { id: 4, url: "Projects", Label: "Projetos" },
+  { id: 5, url: "Skills", Label: "Tecnologias" },
+  { id: 6, url: "Contact", Label: "Contato" },
 ];
