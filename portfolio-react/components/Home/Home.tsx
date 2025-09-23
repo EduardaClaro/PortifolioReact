07f224b57cc6d58bf850263e5684dd04cf8848e3
EduardaClaro <mariaeduardaclaro06@gmali.com>
2025-09-23 11:13:31 -0300

    :iphone: adding new responsiveness

--- portfolio-react/components/Home/Home.tsx
+++ portfolio-react/components/Home/Home.tsx
@@ -5,16 +5,38 @@ import Resume from "./Resume/Resume";
 import Projects from "./Projects/Projects";
 import Skills from "./Skills/Skills";
 import Contact from "./Contact/Contact";
+import ResponsiveNav from "./Navbar/ResponsiveNav";
 
 const Home = () => {
-  return <div className="overflow-hidden h-auto">
-    <Hero/>
-    <Services/>
-    <Resume/>
-    <Projects/>
-    <Skills/>
-    <Contact/>
-  </div>
+  return (
+    <div className="overflow-hidden h-auto">
+      <ResponsiveNav />
+
+      <section id="Hero" className="scroll-mt-[80px]">
+        <Hero />
+      </section>
+
+      <section id="Services" className="scroll-mt-[80px]">
+        <Services />
+      </section>
+
+      <section id="Resume" className="scroll-mt-[80px]">
+        <Resume />
+      </section>
+
+      <section id="Projects" className="scroll-mt-[80px]">
+        <Projects />
+      </section>
+
+      <section id="Skills" className="scroll-mt-[80px]">
+        <Skills />
+      </section>
+
+      <section id="Contact" className="scroll-mt-[80px]">
+        <Contact />
+      </section>
+    </div>
+  );
 };
 
 export default Home;
